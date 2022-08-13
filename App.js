af5a880c07801bcb3d96715b1ffad6f427717c21
Nisha Jha <83752180+Nisha213-mar@users.noreply.github.com>
2022-08-13 11:25:40 +0530

    portfolio

---
+++ App.js
@@ -0,0 +1,39 @@
+const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.controlls');
+const sectBtn = document.querySelectorAll('.control');
+const allSections = document.querySelector('.main-content');
+
+function PageTransitions() {
+    //Button click
+    for (let i = 0; i < sectBtn.length; i++) {
+        sectBtn[i].addEventListener('click', function () {
+            let currentBtn = document.querySelectorAll('.active-btn');
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+            this.className += ' active-btn';
+        })
+    }
+    //sectons active
+    allSections.addEventListener('click', (e) => {
+        const id=e.target.dataset.id;
+        if (id) {
+            //remove selected from other btns
+            sectBtns.forEach((btn) => {
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+            //hide 
+            sections.forEach((section) => {
+               section.classList.remove('active')
+            })
+            const element=document.getElementById(id);
+            element.classList.add('active');
+        }
+    })
+    //Toggle theme
+    const themeBtn= document.querySelector('.theme-btn');
+    themeBtn.addEventListener('click',()=>{
+        let element=document.body;
+        element.classList.toggle('light-mode')
+    })
+}
+PageTransitions();
